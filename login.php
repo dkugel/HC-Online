f347f9f4ef740a25442f306e58dadfc3946f3169
diego <daherrerac@gmail.com>
2018-03-05 16:39:44 -0500

    Initial commit

---
+++ login.php
@@ -0,0 +1,71 @@
+<!doctype html>
+<html lang="en">
+  <head>
+    <!-- Required meta tags -->
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+    <!-- Bootstrap CSS -->
+    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/css/bootstrap.min.css" integrity="sha384-Gn5384xqQ1aoWXA+058RXPxPg6fy4IWvTNh0E263XmFcJlSAwiGgFAW/dAiS6JXm" crossorigin="anonymous">
+    <title>HC-Online</title>
+    <meta name="description" content="HC-Online">    
+    <meta http-equiv="Expires" content="0">
+    <meta http-equiv="Last-Modified" content="0">
+    <meta http-equiv="Cache-Control" content="no-cache, mustrevalidate">
+    <meta http-equiv="Pragma" content="no-cache">
+    <!-- CSS FILES-->    
+    <link rel="stylesheet" href="css/custom.css">
+  </head>
+  <body>
+  <header>
+    <div class="container">
+      <div class="row fit-header">
+        <div class="col">
+          HC:Online Logo
+        </div>
+        <div class="col">
+          Texto de introducción
+        </div>
+        <div class="col">
+          Menú
+        </div>
+      </div>
+    </div>
+  </header>
+  <section>
+    <div class="content-fluid">
+      <div class="row align-items-center">
+        <div class="col">
+          
+        </div>
+        <div class="col login-form">
+          <fieldset>
+            <form>
+                <label>Usuario</label> <br>
+                <input name="user"  type="text" placeholder="Usuario"> <br>
+                <label>Clave</label> <br>
+                <input name="pass"  type="password" placeholder="Clave"> <br>
+                <a href="#">Ingresar</a> <br>
+            </form>
+        </fieldset>
+        </div>
+        <div class="col">
+          
+        </div>
+        
+      </div>
+    </div>
+  </section>
+    <?php
+
+    ?>
+
+    <!-- Optional JavaScript -->
+    <!-- jQuery first, then Popper.js, then Bootstrap JS -->
+    <script src="https://code.jquery.com/jquery-3.2.1.slim.min.js" integrity="sha384-KJ3o2DKtIkvYIK3UENzmM7KCkRr/rE9/Qpg6aAZGJwFDMVNA/GpGFF93hXpG5KkN" crossorigin="anonymous"></script>
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.12.9/umd/popper.min.js" integrity="sha384-ApNbgh9B+Y1QKtv3Rn7W3mgPxhU9K/ScQsAP7hUibX39j7fakFPskvXusvfa0b4Q" crossorigin="anonymous"></script>
+    <script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/js/bootstrap.min.js" integrity="sha384-JZR6Spejh4U02d8jOt6vLEHfe/JQGiRRSQQxSfFWpi1MquVdAyjUar5+76PVCmYl" crossorigin="anonymous"></script>
+  </body>
+</html>
+
+
+
